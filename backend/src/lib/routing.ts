52cd285a93e51bd46ad6aab427760ed91730bd40
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-08-22 20:19:23 -0300

    feat: remove routeEmail function as part of code refactoring

--- backend/src/lib/routing.ts
+++
@@ -1,9 +0,0 @@
-import { EmailState } from "../graph/state/EmailState";
-
-export function routeEmail(state: EmailState): 'spam' | 'legitimate' {
-  if (state.isSpam === true) {
-    return 'spam';
-  } else {
-    return 'legitimate';
-  }
-}
